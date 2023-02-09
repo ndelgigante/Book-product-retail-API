1c3ccf8f3b65c81291e9fae5e73c487efe441a77
Pavithra <iampavithravenkat@gmail.com>
2023-02-09 12:58:58 -0500

    Added models

---
+++ Models/products.py
@@ -0,0 +1,7 @@
+class Product:
+    def __init__(self, productid = None, productname, productdescription, quantity, MSRP):
+        self.productid = productid
+        self.productname = productname
+        self.productdescription = productdescription
+        self.quantity = quantity
+        self.MSRP = MSRPs
