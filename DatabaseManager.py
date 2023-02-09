154ddc0a8ec877ec9d3687eb955580b59dbfc0fc
ndelgigante <nbd@email.sc.edu>
2023-02-09 12:00:12 -0500

    added delete insert and update

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -3,18 +3,45 @@ import mysql.connector
 
 class DatabaseManager:
     def __init__(self):
-        #requires editing to connection to actually access the database
-        conn = mysql.connector.connect(
+        #requires editing of the password and user to connection to actually access the database
+        self.conn = mysql.connector.connect(
         host="localhost",
         user="root",
         password="rootroot",
         database="Book"
         )
-        self.cur = conn.cursor()
+        self.cur = self.conn.cursor()
 
+    #param: 
+    #table_name is a string identifier for the sql table
     def get_df(self,table_name):
-        self.cur.execute(f'SELECT * FROM {table_name}')
+        self.cur.execute(f'SELECT * FROM {table_name};')
         table_rows = self.cur.fetchall()
         df = pd.DataFrame(table_rows)
         return df
+
+    #param: 
+    #table_name is a string identifier for the sql table
+    #values: is a list of the attribute values
+    def insert(self,table_name, values):
+        entry =" "
+        for x in values:
+            entry += ','+x
+        self.cur.execute(f'INSERT INTO {table_name} VALUES({values});')
+        self.conn.commit()
+    
+    #param:
+    #table_name: is a string identifier for the sql table
+    #condition: sql string condition for deletions (ie price < 3)
+    def delete(self,table_name,condition):
+        self.cur.execute(f'DELETE FROM {table_name} WHERE {condition};')
+        self.conn.commit()
+
+
+    #param:
+    #table_name is a string identifier for the sql table
+    #command: is a sql string with the SET and WHERE clause of the update syntax (ie SET column1 = value1, column2 = value2, ...WHERE <condition>) 
+    def update(self,table_name, command):
+        self.cur.execute(f'UPDATE {table_name} {command};')
+        self.conn.commit()
     
